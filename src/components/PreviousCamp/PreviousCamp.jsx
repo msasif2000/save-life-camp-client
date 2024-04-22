a9764ccc285a4e0c11bd233e047be58f83820be4
Asif <msasifiiuc@gmail.com>
2024-04-22 10:04:07 +0600

    previous camp card redesign done

--- src/components/PreviousCamp/PreviousCamp.jsx
+++ src/components/PreviousCamp/PreviousCamp.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import PreviousCampCard from "./PreviousCampCard";
 import { Link } from "react-router-dom";
 import useAxiosPublic from "../../hooks/useAxiosPublic";
+import { BiRightTopArrowCircle } from "react-icons/bi";
 
 
 const PreviousCamp = () => {
@@ -22,14 +23,14 @@ const PreviousCamp = () => {
              <div className="divider mt-16"></div>
             <h2 data-aos="fade-right" className="text-4xl text-center font-bold">Our Previous Camp</h2>
             <p data-aos="fade-left" className="text-xl text-center italic my-2">Click on the details button to see details about our efficient works!</p>
-            <div className="divider"></div>
-            <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-8 mt-8">
+            {/* <div className="divider"></div> */}
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-8 mt-16">
                 {
                     camps.map(camp => <PreviousCampCard key={camp._id} camp={camp}></PreviousCampCard>)
                 }
             </div>
-            <div className="flex justify-center my-4">
-                <Link to='/availableCamp'><button className="btn btn-sm bg-slate-200 text-red-600 border-2 border-red-600">See Available Camps</button></Link>
+            <div data-aos="zoom-in" className="flex justify-end my-8">
+                <Link to='/availableCamp'><button className="btn bg-grn border-2 text-white hover:bg-second"><BiRightTopArrowCircle className="text-2xl"/>Available Camps</button></Link>
             </div>
         </div>
     );
--- src/components/PreviousCamp/PreviousCampCard.jsx
+++ src/components/PreviousCamp/PreviousCampCard.jsx
@@ -1,35 +1,20 @@
-import { BiTimeFive } from "react-icons/bi";
-import { BsFilePerson } from "react-icons/bs";
-import { MdTopic } from "react-icons/md";
+import { BsFillArrowUpRightCircleFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
 
 const PreviousCampCard = ({ camp }) => {
-    const { _id, campName, image, date, services, audience,professionals, participants } = camp;
+    const { _id, campName, image, participants } = camp;
     return (
-        <div className="flex flex-col shadow-2xl shadow-red-600 transition-all hover:-translate-y-1 hover:scale-105 hover:bg-slate-200 duration-300">
-            <img src={image} alt="" className="h-56 w-full" />
-            <p className="bg-red-600 text-white absolute  px-2 mr-4 ">Participated {participants} Peoples</p>
+        <div className="flex flex-col shadow-2xl shadow-third transition-all hover:-translate-y-1 hover:scale-105 hover:bg-slate-200 duration-300 rounded-lg bg-second">
+            <img src={image} alt="" className="h-56 w-full rounded-lg" />
+            <p className="bg-third text-white absolute  px-2 mr-4 rounded-tl-lg">Participated {participants} Peoples</p>
             <div >
                 <div className="">
-                    <p className="font-bold text-3xl py-1 text-center">{campName}</p>
+                    <p className="font-bold text-3xl py-1 text-center text-white">{campName}</p>
                 </div>
-                <div className="flex flex-col text-xl bg-sky-300 p-4 h-32">
-                    <span className="font-bold text-xl text-center">Session Topic</span>
-                    {services?.map((serv, index) => (<p className="flex items-center gap-1" key={index}><MdTopic /> {serv}</p>))}
+                <div data-aos="fade-up" data-aos-duration="1000" className=" px-4 py-2 flex justify-end">
+                    <Link to={`/detailsCamp/${_id}`}><button className="btn border-third bg-grn hover:bg-second text-white"><BsFillArrowUpRightCircleFill className="text-2xl" /> Details</button></Link>
                 </div>
-                <div className=" flex flex-col text-white  bg-red-600 p-4 h-28">
-                    <span className="font-bold text-xl text-center">Guests</span>
-                    {professionals?.map((person, index) => (<p className="flex items-center flex-wrap gap-1" key={index}> <BsFilePerson /> {person}</p>))}
-                </div>
-                <p className="bg-sky-300 flex justify-center gap-1 text-black p-2"><span>Target Audience: </span> <span className="font-bold">{audience}</span></p>
-
-                <div className=" px-4 py-2 flex justify-between">
-                    <p className="flex gap-2 items-center"><BiTimeFive className="text-red-600 text-2xl" /><span className="text-red-600 bold">{date.split('T')[0]}</span></p>
-                    <Link to={`/detailsCamp/${_id}`}><button className="btn border-red-600 bg-sky-300">See Details</button></Link>
-                </div>
-
-
             </div>
         </div>
     );
